cd35855efcab3e1a95f7baccda9727f3e64f1d50
anupong-pakee <anupong.pakee.dev@gmail.com>
2026-01-19 19:49:34 +0700

    feat: add RESTful API endpoints and downgrade Prisma version
    - implement create and read API routes
    - connect API with Prisma client
    - downgrade Prisma to resolve runtime errors

---
+++ app/api/user/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/app/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+    const users = await prisma.user.findMany()
+    return NextResponse.json(users)
+}
+
+export async function POST(req: Request) {
+    const body = await req.json()
+    const user = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email, 
+            password: body.password
+        },
+    })
+    return NextResponse.json(user)
+}
